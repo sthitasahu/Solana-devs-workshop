f846c9dc81739ce50181c5ec01c946759d207e87
sthitasahu <sthitasahu074@gmail.com>
2025-03-25 00:35:27 +0530

    tetsed the contract locally

--- counter/programs/counter/src/lib.rs
+++ counter/programs/counter/src/lib.rs
@@ -1,61 +1,62 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Cfm56JUvw6LnFzb7H744qEJATR6DePVGU9DFdSUfxBAe");
+declare_id!("KLGTnXbsiL7zWNhsoEJGi4K394T3e8XwNtZgAn7Hzpv");
 
-
-
-// Instructions defined in program module
 #[program]
 pub mod counter {
     use super::*;
 
-    // Instruction to initialize a new counter account
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        // Reference to the counter account from the Initialize struct
-        let counter = &ctx.accounts.counter;
+        let counter = &mut ctx.accounts.counter;
+        counter.bump = ctx.bumps.counter; // store bump seed in `Counter` account
         msg!("Counter account created! Current count: {}", counter.count);
+        msg!("Counter bump: {}", counter.bump);
         Ok(())
     }
 
-    // Instruction to increment a counter account
     pub fn increment(ctx: Context<Increment>) -> Result<()> {
-        // Mutable reference to the counter account from the Increment struct
         let counter = &mut ctx.accounts.counter;
         msg!("Previous counter: {}", counter.count);
-
-        // Increment the count value stored on the counter account by 1
         counter.count = counter.count.checked_add(1).unwrap();
         msg!("Counter incremented! Current count: {}", counter.count);
         Ok(())
     }
+    
+    
 }
 
-// Accounts required by the initialize instruction
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    // The account paying to create the counter account
     #[account(mut)]
-    pub user: Signer<'info>, // specify account must be signer on the transaction
+    pub user: Signer<'info>,
 
-    // The counter account being created and initialized in the instruction
+    // Create and initialize `Counter` account using a PDA as the address
     #[account(
-        init,         // specifies we are creating this account
-        payer = user, // specifies account paying for the creation of the account
-        space = 8 + 8 // space allocated to the new account (8 byte discriminator + 8 byte for u64)
+        init,
+        seeds = [b"counter"], // optional seeds for pda
+        bump,                 // bump seed for pda
+        payer = user,
+        space = 8 + Counter::INIT_SPACE
     )]
-    pub counter: Account<'info, Counter>, // specify account is 'Counter' type
-    pub system_program: Program<'info, System>, // specify account must be System Program
+    pub counter: Account<'info, Counter>,
+    pub system_program: Program<'info, System>,
 }
 
-// Account required by the increment instruction
 #[derive(Accounts)]
 pub struct Increment<'info> {
-    #[account(mut)] // specify account is mutable because we are updating its data
-    pub counter: Account<'info, Counter>, // specify account is 'Counter' type
+    // The address of the `Counter` account must be a PDA derived with the specified `seeds`
+    #[account(
+        mut,
+        seeds = [b"counter"], // optional seeds for pda
+        bump = counter.bump,  // bump seed for pda stored in `Counter` account
+    )]
+    pub counter: Account<'info, Counter>,
 }
 
-// Define structure of `Counter` account
+
 #[account]
+#[derive(InitSpace)]
 pub struct Counter {
-    pub count: u64, // define count value type as u64
+    pub count: u64, // 8 bytes
+    pub bump: u8,   // 1 byte
 }
--- counter/tests/counter.ts
+++ counter/tests/counter.ts
@@ -1,7 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Counter } from "../target/types/counter";
-import { Keypair } from "@solana/web3.js";
+import { PublicKey } from "@solana/web3.js";
 
 describe("counter", () => {
   // Configure the client to use the local cluster.
@@ -10,41 +10,38 @@ describe("counter", () => {
 
   const program = anchor.workspace.Counter as Program<Counter>;
 
-  // Generate a new keypair to use as the address the counter account
-  const counterAccount = new Keypair();
+  const [counterPDA] = PublicKey.findProgramAddressSync(
+    [Buffer.from("counter")],
+    program.programId,
+  );
 
   it("Is initialized!", async () => {
-    // Invoke the initialize instruction
-    const transactionSignature = await program.methods
-      .initialize()
-      .accounts({
-        counter: counterAccount.publicKey,
-      })
-      .signers([counterAccount]) // include counter keypair as additional signer
-      .rpc({ skipPreflight: true });
+    try {
+      const txSig = await program.methods
+        .initialize()
+        .accounts({
+          counter: counterPDA,
+        })
+        .rpc();
 
-    // Fetch the counter account data
-    const accountData = await program.account.counter.fetch(
-      counterAccount.publicKey,
-    );
-
-    console.log(`Transaction Signature: ${transactionSignature}`);
-    console.log(`Count: ${accountData.count}`);
+      const accountData = await program.account.counter.fetch(counterPDA);
+      console.log(`Transaction Signature: ${txSig}`);
+      console.log(`Count: ${accountData.count}`);
+    } catch (error) {
+      // If PDA Account already created, then we expect an error
+      console.log(error);
+    }
   });
 
   it("Increment", async () => {
-    // Invoke the increment instruction
     const transactionSignature = await program.methods
       .increment()
       .accounts({
-        counter: counterAccount.publicKey,
+        counter: counterPDA,
       })
       .rpc();
 
-    // Fetch the counter account data
-    const accountData = await program.account.counter.fetch(
-      counterAccount.publicKey,
-    );
+    const accountData = await program.account.counter.fetch(counterPDA);
 
     console.log(`Transaction Signature: ${transactionSignature}`);
     console.log(`Count: ${accountData.count}`);
