163ecdcc5e68c22fc08e4855f90c6e952945aef4
sthitasahu <sthitasahu074@gmail.com>
2025-03-24 23:52:38 +0530

    deployed the smart-contract to devnet

--- smart-contract-counter/programs/smart-contract-counter/src/lib.rs
+++ smart-contract-counter/programs/smart-contract-counter/src/lib.rs
@@ -1,13 +1,15 @@
 use anchor_lang::prelude::*;
 
-declare_id!("EMDedCPiESfktXgEfF5FKgtzTCsPQNiB5A737PB4Nuh9");
+declare_id!("9AMX3iy96Pu2Y7wuHg6aF8dTQDBFvnZmnhpqTFg4DxmC");
 
 #[program]
 mod counter {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         let counter=&mut ctx.accounts.counter;
-        msg!("Counter account created!.Current count: {}", counter.count); // Message will show up in the tx logs
+        counter.bump=ctx.bumps.counter;//store bump in Counter account
+        msg!("Counter account created!.Current count: {}", counter.count);
+        msg!("Counter bump:{}",counter.bump); // Message will show up in the tx logs
         Ok(())
     }
     pub fn increment(ctx:Context<Increment>)->Result<()>{
@@ -33,25 +35,40 @@ pub struct Initialize<'info> {
     // next 8 bytes come from NewAccount.data being type u64.
     // (u64 = 64 bits unsigned integer = 8 bytes)
     #[account(mut)]
-    pub signer:Signer<'info>,
-    #[account(init, payer = signer, space = 8 + 8)]
+    pub user:Signer<'info>,
+
+    //Create and initialize 'Counter' account using PDA as the address
+    #[account(init,
+              seeds=[b"counter"],
+              bump,
+              payer = user, 
+              space = 8 + Counter::INIT_SPACE
+            )]
     pub counter:Account<'info ,Counter>,
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
 pub struct Increment<'info>{
-    #[account(mut)]
+    #[account(mut,
+              seeds=[b"counter"],
+              bump=counter.bump,
+            )]
     pub counter:Account<'info,Counter>,
 }
 
 #[derive(Accounts)]
 pub struct Decrement<'info>{
-    #[account[mut]]
+    #[account(mut,
+             seeds=[b"counter"]
+             bump=counter.bump
+            )]
     pub counter:Account<'info,Counter>,
 }
 
 #[account]
+#[derive(InitSpace)]
 pub struct Counter {
-    count:u64,
+    pub count:u64,//8 bytes
+    pub bump:u8//1 byte
 }
