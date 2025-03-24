050bcec065b29ceacecfad4a82bf93fe268f6add
sthitasahu <sthitasahu074@gmail.com>
2025-03-25 00:07:14 +0530

    initial setup fro counter-contract-logic

--- counter-frontend/src/App.tsx
+++ counter-frontend/src/App.tsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const network=WalletAdapterNetwork.Devnet
+  console.log(network)
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      // if desired, manually define specific/custom wallets here (normally not required)
+      // otherwise, the wallet-adapter will auto detect the wallets a user's browser has available
+    ],
+    [network],
+  );
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <ConnectionProvider endpoint={endpoint}>
+          <WalletProvider wallets={wallets} autoConnect>
+             <WalletModalProvider>
+              <WalletMultiButton/>
+                <h1>Welcome to the counter dapp built on solana</h1>
+             </WalletModalProvider>
+          </WalletProvider>
+       </ConnectionProvider>
+        
     </>
   )
 }
