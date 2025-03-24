5220c64af82bb340bce48ef330b73c3b77dd0b89
sthitasahu <sthitasahu074@gmail.com>
2025-03-24 23:10:09 +0530

    initial frontend setup done

---
+++ counter-frontend/vite.config.ts
@@ -0,0 +1,13 @@
+// WSL USERS ON WINDOWS ONLY (NOT NECESSARY FOR LINUX/MACOS)
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+});
